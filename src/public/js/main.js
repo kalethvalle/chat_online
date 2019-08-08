fd8422e87748a9198e7e7bcfdd871706112b8323
kalethvalle <vallekaleth@gmail.com>
2019-08-08 15:39:26 -0500

    se actualizo toda la interface de forma que se pueda ver los usuarios conectados y quien envia mensajes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,4 +1,4 @@
-// conexion de socket del cliente
+// conexion socket del cliente
 $(function (){
     const socket = io();
 
@@ -7,7 +7,33 @@ $(function (){
     $mensajeForm = $('#mensaje-form');
     $mensajeBox = $('#mensaje');
 
-    // capturando los eventos del DOM
+    // se obtienen los elementos del DOM desde login_form
+    const $loginError = $('#login_error');
+    const $loginForm = $('#login_form');
+    const $loginName = $('#login_name');
+    const $usuarios = $('#nombre_usuarios');
+
+    // captura evento submit del boton del login
+    // para enviar el usuario al servidor <sockets.js>
+    $loginForm.submit(e => {
+        e.preventDefault();
+        socket.emit('nuevo_user', $loginName.val(), cp => {
+            if (cp) {
+                $('#main_login').hide();
+                $('#main_contenedor').show();
+            } else {
+                $loginError.html(`
+                   <div class="alert alert-danger">
+                      Usuario ya esxiste.
+                   </div>
+                `);
+            }
+            $loginName.val('');
+        });
+    });
+
+    // capturando evento submit del boton chat
+    // para enviar el mensaje al servidor <sockets.js>
     $mensajeForm.submit(function (e) {
         e.preventDefault();
         socket.emit('enviando_mensaje', $mensajeBox.val());
@@ -16,6 +42,14 @@ $(function (){
 
     // recibe el mensaje para mostrarlo en el cuerpo del chat uno por uno
     socket.on('nuevo_mensaje', function (nuevo_mensaje){
-       $chat.append(nuevo_mensaje + '<br/>');
+       $chat.append('<b>' + nuevo_mensaje.name + '</b>: ' + nuevo_mensaje.msg + '<br/>');
+    });
+
+    socket.on('usuario', data => {
+        let html = '';
+        for(let i = 0; i < data.length; i++){
+            html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
+        }
+        $usuarios.html(html);
     });
 });
--- src/sockets.js
+++ src/sockets.js
@@ -1,11 +1,38 @@
 // conexion de socket del servidor
 module.exports = function (io) {
+    let usuarios = [];
     io.on('connection', socket => {
-        console.log('nuevo usuario conectado');
+        // console.log('nuevo usuario conectado');
+
+        socket.on('nuevo_user', (nuevo_user, cb) => {
+            console.log(nuevo_user + ' Se ha conectado...');
+            if (usuarios.indexOf(nuevo_user) != -1){
+                cb(false);
+            }else{
+                cb(true);
+                socket.users = nuevo_user;
+                usuarios.push(socket.users);
+                actualizaUsuarios();
+            }
+        });
 
         // recibe el mensaje del $mensajeForm
         socket.on('enviando_mensaje', function (enviando_mensaje){
-            io.sockets.emit('nuevo_mensaje', enviando_mensaje);
+            io.sockets.emit('nuevo_mensaje', {
+                name: socket.users,
+                msg: enviando_mensaje
+            });
         });
+
+        socket.on('disconnect', user => {
+            if(!socket.users) return;
+            console.log(socket.users + ' Se ha desconectado...');
+            usuarios.splice(usuarios.indexOf(socket.users), 1);
+            actualizaUsuarios();
+        });
+
+        function actualizaUsuarios() {
+            io.sockets.emit('usuario', usuarios);
+        }
     });
 }
