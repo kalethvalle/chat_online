2f764b180d268bb58e8c0fb3a584a627395b8f2a
kalethvalle <vallekaleth@gmail.com>
2019-10-04 16:27:39 -0500

    04/10/2019 actualizo el diseño

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,5 @@
 // conexion socket del cliente
-$(function () {
+$(function() {
     const socket = io();
 
     // se obtienen los elementos del DOM desde la interface
@@ -35,7 +35,7 @@ $(function () {
 
     // capturando evento submit del boton chat
     // para enviar el mensaje al servidor <sockets.js>
-    $mensajeForm.submit(function (e) {
+    $mensajeForm.submit(function(e) {
         e.preventDefault();
         // audioEnviaMensaje();
         socket.emit('enviando_mensaje', $mensajeBox.val(), cb => {
@@ -47,7 +47,7 @@ $(function () {
     });
 
     // recibe el mensaje para mostrarlo en el cuerpo del chat uno por uno
-    socket.on('nuevo_mensaje', function (nuevo_mensaje) {
+    socket.on('nuevo_mensaje', function(nuevo_mensaje) {
         console.log(nuevo_mensaje.userIndex + '->' + nuevo_mensaje.name)
         if (nuevo_mensaje.userIndex === nuevo_mensaje.name) {
             audioEnviaMensaje();
@@ -55,24 +55,27 @@ $(function () {
         } else {
             $chat.append('<b>' + nuevo_mensaje.name + '</b>: ' + nuevo_mensaje.msg + '<br/>');
         }
+        $("#chat").animate({ scrollTop: $('#chat')[0].scrollHeight }, 100);
     });
 
-    socket.on('personal', function (msj_personal) {
+    socket.on('personal', function(msj_personal) {
         $chat.append(`
            <p class="personal"><b>${msj_personal.name}:</b> ${msj_personal.msj}</p>
         `);
+        $("#chat").animate({ scrollTop: $('#chat')[0].scrollHeight }, 1000);
     });
 
     // muestra en el front todos los usuarios que estan conectados
     socket.on('usuario', data => {
         let html = '';
         for (let i = 0; i < data.length; i++) {
-            html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
+            html += `<li class="list-group-item"><img src="https://image.freepik.com/vector-gratis/perfil-icono-hombre-avatar-hipster-hombre-use-auriculares_48369-8728.jpg" alt="Avatar" class="avatar"></i> ${data[i]}</li>`
         }
 
         $usuarios.html(html);
     });
 });
+
 
 function audioEnviaMensaje() {
     document.getElementById('sonido').play();
